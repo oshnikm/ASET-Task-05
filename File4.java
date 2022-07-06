f36f87fd4ec7c50fd815134f3159d3fea91c2696
oshnikm <106856676+oshnikm@users.noreply.github.com>
2022-07-06 19:14:42 +0530

    File4.java

---
+++ File4.java
@@ -0,0 +1,19 @@
+package FP_Experiments;
+
+/**
+ * @author Oshni M
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class File4 {
+	public static void main(String[] args)throws Exception{
+		BufferedReader r = new BufferedReader(new FileReader("C:/Users/Oshni M/eclipse-workspace/oshni/repository6/repository6/Task 05/data/Flowers.txt"));
+		r.lines()
+		.map(l->l.toUpperCase())
+		.forEach(l->System.out.println(l));
+		r.close();
+	}
+
+}
