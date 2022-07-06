e9829d484ade7e445ab0e50a6d699585c6e74cc5
oshnikm <106856676+oshnikm@users.noreply.github.com>
2022-07-06 19:21:01 +0530

    GUI.java

---
+++ GUI.java
@@ -0,0 +1,25 @@
+package FP_Experiments;
+
+/**
+ *@author Oshni M
+ */
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class GUI {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		JButton b = new JButton("Press Me");
+		f.setLayout(new BorderLayout());
+		f.add(b,BorderLayout.CENTER);
+		b.addActionListener(
+				event -> System.out.println("Button...." + event.getActionCommand()));
+		
+		f.pack();
+		
+		f.setVisible(true);
+	}
+}
