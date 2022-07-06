7321bfa7451a9748b24108ceb5596bc954cf909d
oshnikm <106856676+oshnikm@users.noreply.github.com>
2022-07-06 19:19:05 +0530

    Flower02.java

---
+++ Flower02.java
@@ -0,0 +1,27 @@
+package FP_Experiments;
+
+/**
+ * @author Oshni M
+ */
+
+import java.util.*;
+
+public class Flower02 {
+  public static void main(String[] args) {
+	    List<Flower> table = Arrays.asList(
+	            new Flower(1,"Red","Rose", 100),
+	            new Flower(2,"White", "Lilly", 150),
+	            new Flower(3,"Purple", "Tulip", 240),
+	            new Flower(4,"Green", "Orchid", 50),
+	            new Flower(5,"Hot Pink", "Carnation", 180),
+	            new Flower(6,"Indigo", "Iris",100),
+	            new Flower(7,"Yellow", "Daffodil",140),
+	            new Flower(8,"Blue", "Daisy", 180),
+	            new Flower(9,"Orange", "Marigold", 20),
+	            new Flower(10,"Pink", "Araliya",60));
+
+    table.stream().forEach(x -> System.out.println(x));
+    System.out.println();
+    table.parallelStream().forEach(System.out::println);
+  }
+}
